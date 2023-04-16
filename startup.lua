74a4e5f55d477bc9b9191ccf9bebc676abb88df2
minecartchris <130516132+minecartchris@users.noreply.github.com>
2023-04-15 22:19:02 -0500

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -6,13 +6,13 @@ term.setCursorPos(1, 1)
 -- Variables
 local idTable = {
   [10] = "Ryan,"
-  [56] = "Cheesy,"
-  [31] = "Chris,"
-  [16] = "Jeremy,"
-  [40] = "Hunter,"
-  [73] = "Chris' basement,"
-  [83] = "Chris' hacking tablet,"
-  [97] = "aleeya's coputer,"
+  [56] = "Cheesy,",
+  [31] = "Chris,",
+  [16] = "Jeremy,",
+  [40] = "Hunter,",
+  [73] = "Chris' basement,",
+  [83] = "Chris' hacking tablet,",
+  [97] = "aleeya's coputer,",
 }
 
 -- Main Code
